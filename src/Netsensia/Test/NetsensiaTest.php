944f23438ee59d4d4328ec2f73b0c773b5a7c9e4
Chris Moreton <chris@netsensia.com>
2013-11-18 20:24:38 +0000

    Add Mockery as dependency - add Adaptor mock to Test class

--- src/Netsensia/Test/NetsensiaTest.php
+++ src/Netsensia/Test/NetsensiaTest.php
@@ -31,14 +31,48 @@ class NetsensiaTest extends PHPUnit_Framework_TestCase
     
         $authService = $this->getMock('Zend\Authentication\AuthenticationService');
         $authService->expects($this->any())
-        ->method('getIdentity')
-        ->will($this->returnValue($userSessionModel));
+            ->method('getIdentity')
+            ->will($this->returnValue($userSessionModel));
     
         $authService->expects($this->any())
-        ->method('hasIdentity')
-        ->will($this->returnValue(true));
+            ->method('hasIdentity')
+            ->will($this->returnValue(true));
         
         $this->getServiceLocator()->setAllowOverride(true);
         $this->getServiceLocator()->setService('Zend\Authentication\AuthenticationService', $authService);
-    }    
+    }
+
+    protected function getAdapterMock()
+    {
+        $adapter = \Mockery::mock('Zend\Db\Adapter\Adapter');
+        $platform = \Mockery::mock('Zend\Db\Adapter\Platform\Mysql[getName]');
+        $stmt = \Mockery::mock('Zend\Db\Adapter\Driver\Pdo\Statement');
+        $paramContainer = \Mockery::mock('Zend\Db\Adapter\ParameterContainer');
+    
+        $platform->shouldReceive('getName')
+            ->once()
+            ->andReturn('MySQL');
+    
+        $stmt->shouldReceive('getParameterContainer')
+            ->once()
+            ->andReturn($paramContainer);
+    
+        $stmt->shouldReceive('setSql')
+            ->once()
+            ->andReturn($stmt);
+    
+        $stmt->shouldReceive('execute')
+            ->once()
+            ->andReturn(array());
+    
+        $adapter->shouldReceive('getPlatform')
+            ->once()
+            ->andReturn($platform);
+    
+        $adapter->shouldReceive('createStatement')
+            ->once()
+            ->andReturn($stmt);
+    
+        return $adapter;
+    }
 }
