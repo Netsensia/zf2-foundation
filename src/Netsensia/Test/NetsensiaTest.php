e76a7cd84bf6719f14234941cb5a1deaa417871b
Chris Moreton <chris@netsensia.com>
2013-11-18 20:56:28 +0000

    Remove DB adapter mock

--- src/Netsensia/Test/NetsensiaTest.php
+++ src/Netsensia/Test/NetsensiaTest.php
@@ -42,37 +42,4 @@ class NetsensiaTest extends PHPUnit_Framework_TestCase
         $this->getServiceLocator()->setService('Zend\Authentication\AuthenticationService', $authService);
     }
 
-    protected function getAdapterMock()
-    {
-        $adapter = \Mockery::mock('Zend\Db\Adapter\Adapter');
-        $platform = \Mockery::mock('Zend\Db\Adapter\Platform\Mysql[getName]');
-        $stmt = \Mockery::mock('Zend\Db\Adapter\Driver\Pdo\Statement');
-        $paramContainer = \Mockery::mock('Zend\Db\Adapter\ParameterContainer');
-    
-        $platform->shouldReceive('getName')
-            ->once()
-            ->andReturn('MySQL');
-    
-        $stmt->shouldReceive('getParameterContainer')
-            ->once()
-            ->andReturn($paramContainer);
-    
-        $stmt->shouldReceive('setSql')
-            ->once()
-            ->andReturn($stmt);
-    
-        $stmt->shouldReceive('execute')
-            ->once()
-            ->andReturn(array());
-    
-        $adapter->shouldReceive('getPlatform')
-            ->once()
-            ->andReturn($platform);
-    
-        $adapter->shouldReceive('createStatement')
-            ->once()
-            ->andReturn($stmt);
-    
-        return $adapter;
-    }
 }
